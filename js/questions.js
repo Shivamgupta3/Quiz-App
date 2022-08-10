d08f23d96aab819af7b39522bbadd3f52ba521c2
Shivamgupta3 <110226168+Shivamgupta3@users.noreply.github.com>
2022-08-11 03:32:20 +0530

    Quiz App

---
+++ js/questions.js
@@ -0,0 +1,80 @@
+let MCQS = [{
+    question: "What does HTML stand for?",
+    choice1: "Hyperlinks and Text Markup Language",
+    choice2: "Hyper Text Markup Language",
+    choice3: "Hyper Text Making Language",
+    choice4: "Hyper Text Mark Language",
+    answer: 1
+},
+{
+    question: "Which of the following HTML tag is used to create an unordered list?",
+    choice1: "<ol>",
+    choice2: "<li>",
+    choice3: "<ul>",
+    choice4: "<ll>",
+    answer: 2
+},
+{
+    question: "Colors are defined in HTML using?",
+    choice1: "RGB values",
+    choice2: "HEX values",
+    choice3: "RGBA values",
+    choice4: "All of the above",
+    answer: 3
+},
+{
+    question: "What does CSS stand for?",
+    choice1: "Cascading style sheets",
+    choice2: "Color and style sheets",
+    choice3: "Creative style sheet",
+    choice4: "None of the above",
+    answer: 0
+},
+{
+    question: "Which is the correct CSS syntax?",
+    choice1: "{body{color:black}",
+    choice2: "body{color:black}",
+    choice3: "body={color:black}",
+    choice4: "body:color{black}",
+    answer: 1
+},
+{
+    question: "Which of the below is the correct syntax to put a line over text in CSS?",
+    choice1: "text-decoration: line",
+    choice2: "text-decoration: none",
+    choice3: "text-decoration: overline",
+    choice4: "text-decoration: underline",
+    answer: 2
+},
+{
+    question: "Which of the following is correct about JavaScript?",
+    choice1: "JavScript is an Assembly language",
+    choice2: "JavScript is an High-level language",
+    choice3: "JavScript is an Object-Based language",
+    choice4: "JavScript is an Object-Oriented language",
+    answer: 2
+},
+{
+    question: "Inside which HTML element do we put the JavaScript?",
+    choice1: "<js></js>",
+    choice2: "<javascript></javascript>",
+    choice3: "<script></script>",
+    choice4: "<scripting>",
+    answer: 2
+},
+{
+    question: "How to write an IF statement in JavaScript?",
+    choice1: "if i==5",
+    choice2: "if(i==5)",
+    choice3: "if(i==5)then",
+    choice4: "if i==5 then",
+    answer: 2
+},
+{
+    question: "How does a WHILE loop start?",
+    choice1: "while(i <= 0)",
+    choice2: "while(i <= 0 i++)",
+    choice3: "while i <= 0",
+    choice4: "while (i++ i <= 0)",
+    answer: 0
+}];
